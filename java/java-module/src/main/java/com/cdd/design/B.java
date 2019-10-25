43a78476b12f2842c004ac76744bec34348211f8
cuoduidui <780106788@qq.com>
2019-10-25 17:11:55 +0800

    //静态方法 静态属性 属于类的本身 所以子类不会继承父类的静态属性和方法 但是可以使用 //子类中有与父类相同的静态属性和方法时  互不影响

--- java/java-module/src/main/java/com/cdd/design/B.java
+++ java/java-module/src/main/java/com/cdd/design/B.java
@@ -8,7 +8,9 @@ public class B extends A {
     public static void main(String[] args) {
         //静态方法 静态属性 属于类的本身 所以子类不会继承父类的静态属性和方法 但是可以使用
         //子类中有与父类相同的静态属性和方法时  互不影响
-        B b = new B();
 
+        B b = new B();
+        //可以直接用方法
+        b();
     }
 }
