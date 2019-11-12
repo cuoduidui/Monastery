e8eaec208ac2498f2b258ba086137738ba21b238
cuoduidui <780106788@qq.com>
2019-11-12 10:57:24 +0800

    SpringBootApplication不一定非要注释在启动类上面

---
+++ spring/spring-boot/src/main/java/com/cdd/springboot/Test.java
@@ -0,0 +1,15 @@
+package com.cdd.springboot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 测试springbootapplication注解是否只能在启动类上
+ * SpringBootApplication不一定非要注释在启动类上面
+ *
+ * @author cuoduidui
+ * @AliasFor spring4.1后提供的
+ * @date 2019-11-11 23:55
+ **/
+@SpringBootApplication
+public class Test {
+}
