0e584bdadb790f9b4b38973778181686e10b1d75
cuoduidui <780106788@qq.com>
2019-11-12 13:52:14 +0800

    AliasFor spring4.2开始提供的，可以把其他注解的属性指定到本注解上

--- spring/spring-boot/src/main/java/com/cdd/springboot/Test.java
+++ spring/spring-boot/src/main/java/com/cdd/springboot/Test.java
@@ -1,13 +1,15 @@
 package com.cdd.springboot;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.annotation.AliasFor;
 
 /**
  * 测试springbootapplication注解是否只能在启动类上
  * SpringBootApplication不一定非要注释在启动类上面
+ * <p>
+ * {@link AliasFor spring4.2开始提供的，可以把其他注解的属性指定到本注解上
  *
  * @author cuoduidui
- * @AliasFor spring4.1后提供的
  * @date 2019-11-11 23:55
  **/
 @SpringBootApplication
