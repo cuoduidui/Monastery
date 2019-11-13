3fb3f0a6eb7dc32362c50343da0646f77cdf5b26
cuoduidui <780106788@qq.com>
2019-11-13 11:37:14 +0800

    如果springbootapplication注解不打到main方法的启动类上 SpringApplication.run(Test.class, args); 相应的run方法参数改成打标注解的类

--- spring/spring-boot/src/main/java/com/cdd/springboot/Application.java
+++ spring/spring-boot/src/main/java/com/cdd/springboot/Application.java
@@ -2,12 +2,12 @@ package com.cdd.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.stereotype.Controller;
 //@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Test.class, args);
     }
 
 }
