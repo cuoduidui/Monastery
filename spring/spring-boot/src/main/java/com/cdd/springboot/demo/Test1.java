7120c08e79aca0abe27a34bfc7a429cb09a4ea81
cuoduidui <780106788@qq.com>
2019-11-18 17:09:15 +0800

    如果springbootapplication注解不打到main方法的启动类上 SpringApplication.run(Test.class, args); 相应的run方法参数改成打标注解的类

---
+++ spring/spring-boot/src/main/java/com/cdd/springboot/demo/Test1.java
@@ -0,0 +1,16 @@
+package com.cdd.springboot.demo;
+
+import org.apache.tomcat.util.http.fileupload.MultipartStream;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+import java.lang.annotation.Inherited;
+
+/**
+ * @author cuoduidui
+ * @date 2019-11-13 17:32
+ **/
+@Inherited
+public @interface Test1{
+}
